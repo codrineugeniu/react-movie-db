7010946263218fb01056752051d351ad91753ba4
Codrin <codrin.baleanu@gmail.com>
2018-08-27 18:43:36 +0300

    Add ui for the movie list app

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import MovieItem from './MovieItem'
 
+import './MovieList.css'
 
 function MovieList() {
   return (
-    <div>
-      <div>
-        Deadpool 2
+    <div className="movie-list">
+      <div className="movies-container">
+        <MovieItem
+          title="Independence Day"
+          year="1996"
+          description="The aliens are coming and their goal is to invade and destroy Earth. Fighting superior technology, mankind's best weapon is the will to survive."
+        />
       </div>
       <Button variant="contained" color="primary">
         Add new movie
@@ -15,4 +21,4 @@ function MovieList() {
   )
 }
 
-export default MovieList;
+export default MovieList
