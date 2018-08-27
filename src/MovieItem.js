ed42a679aef4cdb6187525962b325c65c8291c07
Codrin <codrin.baleanu@gmail.com>
2018-08-27 20:44:47 +0300

    Add axios, persist movie list

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -2,17 +2,18 @@ import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import { checkPropTypes } from 'prop-types'
 
 const MovieItem = props => {
+  const { title, year, description } = props
+
   return (
     <Card className="movie-item">
       <CardContent>
         <Typography variant="headline" component="h2">
-          {props.title}
+          {title}
         </Typography>
-        <Typography color="textSecondary">{props.year}</Typography>
-        <Typography component="p">{props.description}</Typography>
+        <Typography color="textSecondary">{year}</Typography>
+        <Typography component="p">{description}</Typography>
       </CardContent>
     </Card>
   )
--- src/MovieList.js
+++ src/MovieList.js
@@ -4,15 +4,21 @@ import MovieItem from './MovieItem'
 
 import './MovieList.css'
 
-function MovieList() {
+function MovieList(props) {
+  const { movies = [] } = props
   return (
     <div className="movie-list">
       <div className="movies-container">
-        <MovieItem
-          title="Independence Day"
-          year="1996"
-          description="The aliens are coming and their goal is to invade and destroy Earth. Fighting superior technology, mankind's best weapon is the will to survive."
-        />
+        {movies.length > 0
+          ? movies.map(movie => (
+              <MovieItem
+                title={movie.name}
+                year={movie.year}
+                description={movie.description}
+                key={movie.id}
+              />
+            ))
+          : 'No movies found. Perhaps add one?'}
       </div>
       <Button variant="contained" color="primary">
         Add new movie
